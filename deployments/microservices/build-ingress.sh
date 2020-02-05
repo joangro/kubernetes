05c33879b05645ddfa0baf91bd24973f906f7fce
joangro <jgraunoel@gmail.com>
2020-02-05 16:07:58 +0100

    Added ingress configuration files

---
+++ deployments/microservices/build-ingress.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+NUMBER_OF_DEPLOYMENTS=$1
+if [ -z $NUMBER_OF_DEPLOYMENTS ];then
+        echo "Number of deployments weren't provided. Pass it as first argument when executing the script."
+        exit -1
+fi
+
+cp ingress-dynamic.yaml ingress-dynamic.old
+
+for i in $(eval echo "{1..$NUMBER_OF_DEPLOYMENTS}")
+do
+        PORT_TO_EXPOSE=$(( $i + 8000 ))
+	cat <<EOF >> ingress-dynamic.yaml
+         - path: /service1
+           backend:
+             serviceName: microservice-$i
+             servicePort: $PORT_TO_EXPOSE
+EOF
+
+done
+
+echo "Applying configuration..."
+kubectl apply -f ingress-dynamic.yaml
+
+echo -e "Applied ingress configuration:\n"
+cat ingress-dynamic.yaml
+
+echo -e "\nResetting ingress-dynamic.yaml to its original contents..."
+mv ingress-dynamic.old ingress-dynamic.yaml
+
+echo "Done!"
---
+++ deployments/microservices/setup-ingress.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+# Based on https://docs.nginx.com/nginx-ingress-controller/installation/installation-with-manifests/
+
+echo "Kubernetes cluster info:"
+kubectl cluster-info
+
+echo "Setting up ingress on the cluster $(kubectl config current-context)..."
+git clone https://github.com/nginxinc/kubernetes-ingress/
+git checkout v1.6.1
+cd kubernetes-ingress/deployments
+
+# Configure RBAC
+kubectl apply -f common/ns-and-sa.yaml
+kubectl apply -f rbac/rbac.yaml
+
+# Create Default secret, customization ConfigMap and Custom Resource Definitions
+kubectl apply -f common/default-server-secret.yaml
+kubectl apply -f common/nginx-config.yaml
+kubectl apply -f common/custom-resource-definitions.yaml
+
+# Run ingress controller
+kubectl apply -f deployment/nginx-ingress.yaml
+kubectl get pods --namespace=nginx-ingress
+
+# Create GCP Load Balancer
+kubectl apply -f service/loadbalancer.yaml
+
+rm -r ../../kubernetes-ingress
+
